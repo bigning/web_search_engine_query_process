5828dd29632caa9abb88368455d3d32da8a756e0
bigning <bigning1118@gmail.com>
2016-11-16 20:44:14 -0500

    2.0

--- search_engine.cpp
+++ search_engine.cpp
@@ -8,17 +8,24 @@
 #include <algorithm>
 #include <iterator>
 #include <set>
+#include <ctime>
 
 SearchEngine::SearchEngine() {
-    data_path_ = "../data/postings_from_doc_small/";
+    data_path_ = "../data/postings_from_doc/";
     top_k_ = 20;
     new_text_file_.open((data_path_ + "new_text").c_str(), std::ios::binary);
+    show_running_time_ = true;
 }
 
 void SearchEngine::run() {
     inverted_index_file_.open((data_path_ + "inverted_list").c_str(),
             std::ifstream::binary);
+    std::clock_t begin = clock();
     read_in_words_and_urls();
+    std::clock_t end = clock();
+    if (show_running_time_) {
+        std::cout << "[TIME]: " << double(end - begin) / CLOCKS_PER_SEC << std::endl;;
+    }
 
     while (true) {
         Query query = get_query();
@@ -46,6 +53,7 @@ void SearchEngine::preprocess_query(Query& query) {
         words.push_back(query.words[i]);
     }
     query.words = words;
+    bm25_score_left_cache_.clear();
 }
 
 void SearchEngine::process_conjunctive_query(Query& query) {
@@ -53,6 +61,8 @@ void SearchEngine::process_conjunctive_query(Query& query) {
         std::cout << "[WARNING]: no valid query word" << std::endl;
         return;
     }
+    
+    std::clock_t begin = clock();
     std::vector<PostingList> posting_list_vec(query.words.size());
     for (int i = 0; i < query.words.size(); i++) {
         if (!open_list(query.words[i], posting_list_vec[i])) {
@@ -60,12 +70,21 @@ void SearchEngine::process_conjunctive_query(Query& query) {
                 << query.words[i] << std::endl;
         }
     }
+    std::clock_t end = clock();
+    if (show_running_time_) {
+        std::cout << "[TIME]: loading postings " <<
+            double(end - begin) / (double)(CLOCKS_PER_SEC) << std::endl;;
+    }
 
+    begin = clock();
     std::vector<QueryRes> query_results;
     int did = 0;
     int d = 0;
     std::vector<bool> is_valid(posting_list_vec.size(), true);
+    std::clock_t begin_nest, end_nest;
+    double time_next_geq = 0, time_generate_query_res = 0, time_insert= 0;
     while (did < doc_num_) {
+        begin_nest = clock();
         did = next_geq(posting_list_vec[0], did);
         if (did >= doc_num_) break;
         for (int i = 1; i < query.words.size(); i++) { 
@@ -73,19 +92,39 @@ void SearchEngine::process_conjunctive_query(Query& query) {
             d = next_geq(posting_list_vec[i], did);
             if (d != did) break;
         }
+        end_nest = clock();
+        time_next_geq += double(end_nest - begin_nest) / double(CLOCKS_PER_SEC);
         if (d > did) { 
             did = d; /* not in intersection */
             if (did >= doc_num_) break;
         } else {
+            begin_nest = clock();
             QueryRes query_res = generate_query_res(posting_list_vec, query, did,
                     is_valid);
+            end_nest = clock();
+            time_generate_query_res += double(end_nest - begin_nest) / double(CLOCKS_PER_SEC);
+
+            begin_nest = clock();
             insert_into_top_k(query_results, query_res);
+            end_nest = clock();
+            time_insert += double(end_nest - begin_nest) / double(CLOCKS_PER_SEC);
             did++;
         }
     }
     //std::cout << query_results.size() << std::endl;
+    end = clock();
+
     display_results(query, query_results);
     std::cout << std::endl;
+
+    if (show_running_time_) {
+        std::cout << "[TIME]: get conjunctive results " <<
+            double(end - begin) / (double)(CLOCKS_PER_SEC) << std::endl;;
+
+        std::cout << "[TIME]: next_geq " << time_next_geq << std::endl;
+        std::cout << "[TIME]: generate_query_res " << time_next_geq << std::endl;
+        std::cout << "[TIME]: insertion " << time_insert << std::endl;
+    }
 }
 
 void SearchEngine::process_disjunctive_query(Query& query) {
@@ -100,7 +139,12 @@ void SearchEngine::process_disjunctive_query(Query& query) {
                 << query.words[i] << std::endl;
         }
     }
+    int posting_list_vec_size = posting_list_vec.size();
     std::vector<QueryRes> query_results;
+
+    std::clock_t begin_nest, end_nest;
+    double time_next_geq = 0, time_generate_query_res = 0, time_insert= 0, time_find_min = 0;
+    std::clock_t begin = clock();
 
     int current_mimimum = doc_num_;
     for (int i = 0; i < query.words.size(); i++) {
@@ -109,49 +153,72 @@ void SearchEngine::process_disjunctive_query(Query& query) {
         }
     }
     int next_mimimum = doc_num_;
+    int process_num = 0;
     while (current_mimimum < doc_num_) {
-        std::vector<PostingList> tmp_posting_list_vec;
-        std::vector<bool> is_valid;
+        int valid_num = 0;
+        std::vector<bool> is_valid(posting_list_vec_size, false);
         int doc_id = 0;
-        for (int i = 0; i < posting_list_vec.size(); i++) {
-            doc_id = next_geq(posting_list_vec[i], current_mimimum);
+        begin_nest = clock();
+        for (int i = 0; i < posting_list_vec_size; i++) {
+            doc_id = next_geq_linear(posting_list_vec[i], current_mimimum);
             if (doc_id == current_mimimum) {
-                tmp_posting_list_vec.push_back(posting_list_vec[i]);
-                is_valid.push_back(true);
+                valid_num++;
+                //is_valid.push_back(true);
+                is_valid[i] = true;
+                if (posting_list_vec[i].current + 1 < posting_list_vec[i].postings.size() &&
+                        posting_list_vec[i].postings[posting_list_vec[i].current + 1].doc_id <
+                        next_mimimum) {
+                    next_mimimum =
+                        posting_list_vec[i].postings[posting_list_vec[i].current + 1].doc_id;
+                }
             } else {
                 if (doc_id < next_mimimum) {
                     next_mimimum = doc_id;
                 }
-                is_valid.push_back(false);
+                //is_valid.push_back(false);
             }
         }
+        end_nest = clock();
+        time_next_geq += (double)(end_nest - begin_nest) / (double)CLOCKS_PER_SEC;
 
-        if (tmp_posting_list_vec.size() > 0) {
+        if (valid_num > 0) {
+            begin_nest = clock();
             QueryRes query_res = generate_query_res(posting_list_vec, 
                     query, current_mimimum, is_valid);
+            end_nest = clock();
+            time_generate_query_res += (double)(end_nest - begin_nest) / (double)CLOCKS_PER_SEC;
+
+            begin_nest = clock();
             insert_into_top_k(query_results, query_res);
-        }
-        if (tmp_posting_list_vec.size() == posting_list_vec.size()) {
-            next_mimimum = doc_num_;
-            for (int i = 0; i < query.words.size(); i++) {
-                if (posting_list_vec[i].current + 1 < 
-                     posting_list_vec[i].postings.size() &&
-                     posting_list_vec[i].
-                     postings[posting_list_vec[i].current + 1].doc_id
-                     < next_mimimum) {
-                    next_mimimum = posting_list_vec[i].postings
-                        [posting_list_vec[i].current + 1].doc_id;
-                }
-            }
+            end_nest = clock();
+            time_insert += (double)(end_nest - begin_nest) / (double)CLOCKS_PER_SEC;
         }
 
         current_mimimum = next_mimimum;
         next_mimimum = doc_num_;
+        process_num++;
+        /*
+        if (process_num % 10000 == 0 && show_running_time_) {
+            std::cout << "[TIME]: next_get " << time_next_geq << std::endl;
+            std::cout << "[TIME]: insert " << time_insert << std::endl;
+            std::cout << "[TIME]: generate_query_res " << time_generate_query_res << std::endl;
+        }
+        */
     }
+    std::clock_t end = clock();
 
     std::cout << "results: " << query_results.size() << std::endl;
     display_results(query, query_results);
     std::cout << std::endl;
+
+    if (show_running_time_) {
+        std::cout << "[TIME]: disjunctive process " 
+            << (double)(end - begin) / (double)CLOCKS_PER_SEC << std::endl;
+        std::cout << "[TIME]: next_get " << time_next_geq << std::endl;
+        std::cout << "[TIME]: insert " << time_insert << std::endl;
+        std::cout << "[TIME]: generate_query_res " << time_generate_query_res << std::endl;
+        std::cout << "[TIME]: find_min " << time_find_min << std::endl;
+    }
 }
 
 void SearchEngine::display_results(Query& query, std::vector<QueryRes>& results) {
@@ -222,11 +289,30 @@ void SearchEngine::display_results(Query& query, std::vector<QueryRes>& results)
 
 void SearchEngine::insert_into_top_k(std::vector<QueryRes>& results,
         QueryRes& res) {
-    results.push_back(res);
-    std::sort(results.begin(), results.end());
-    if (results.size() > top_k_) {
-        std::vector<QueryRes> tmp_res(results.begin(), results.begin() + top_k_);
-        results = tmp_res;
+    if (results.size() == 0) {
+        results.push_back(res);
+        return;
+    }
+  
+    int results_size = results.size();
+    if (results_size >= top_k_ && res.score < results[results_size - 1].score) {
+        return;
+    }
+
+    bool inserted = false;
+    std::vector<QueryRes>::iterator iter = results.begin();
+    for (; iter != results.end(); iter++) {
+        if (iter->score < res.score) {
+            results.insert(iter, res);
+            inserted = true;
+            break;
+        }
+    }
+    if (!inserted) {
+        results.insert(results.end(), res);
+    }
+    if (results_size >= top_k_) {
+        results.erase(results.end());
     }
 }
 
@@ -245,23 +331,35 @@ QueryRes SearchEngine::generate_query_res(
     float b = 0.75;
     float N = (float)doc_num_;
     float score = 0;
-    float d = (float)(docs_[doc_id].length);
+    int d = (docs_[doc_id].length);
+    std::map<int, float>::iterator iter;
+    float K = 0;
+    if (d < K_CACHE_SIZE) {
+        K = K_[d];
+    } else {
+        K = k1 * ((1 - b) + b * (float)d / average_doc_length_);
+    }
     for (int i = 0; i < query.words.size(); i++) {
         if (!is_valid[i]) {
             continue;
         }
-        float tmp_score = 0;
-        float ft = (float)(words_[query.words[i]].num_of_doc_contain_this_word);
-        float left = log((N - ft + 0.5) / (ft + 0.5));
+        float left = 0;
+
+        iter = bm25_score_left_cache_.find(i);
+        if (iter == bm25_score_left_cache_.end()) {
+            float ft = (float)(words_[query.words[i]].num_of_doc_contain_this_word);
+            left = log((N - ft + 0.5) / (ft + 0.5));
+            bm25_score_left_cache_.insert(std::pair<int, float>(i, left));
+        } else {
+            left = bm25_score_left_cache_[i];
+        }
 
         float fdt = (float)(res.pos[i].size());
         float right_up = (k1 + 1) * fdt;
-        float K = k1 * ((1 - b) + b * d / average_doc_length_);
         float right_down = K + fdt;
         float right = right_up / right_down;
 
-        tmp_score = left * right;
-        score += tmp_score;
+        score += left * right;
     }
     res.score = score;
     return res;
@@ -290,13 +388,15 @@ void SearchEngine::read_in_words_and_urls() {
     int num = 0;
     std::string previous_word;
     while (words_info_file >> word >> pos >> num) {
+        /*
         WordInfo word_info;
         word_info.word = word;
         word_info.start_position = pos;
         word_info.end_position = 0;
         word_info.num_of_doc_contain_this_word = num;
+        */
 
-        words_.insert(std::pair<std::string, WordInfo>(word, word_info));
+        words_.insert(std::pair<std::string, WordInfo>(word, WordInfo(word, pos, 0, num)));
         if (previous_word.size() > 0) {
             words_[previous_word].end_position = pos;
         }
@@ -309,21 +409,33 @@ void SearchEngine::read_in_words_and_urls() {
 
     words_info_file.close();
 
+    int max_doc_num = 10000000;
+    int real_doc_num = 0;
+    DocInfo doc_info_tmp;
+    docs_.resize(max_doc_num, doc_info_tmp);
     std::string docs_filename = data_path_ + "urltable";
     std::ifstream docs_file(docs_filename.c_str());
     int doc_id = 0;
     int length = 0;
     std::string url, data_file_name;
     while (docs_file >> doc_id >> length >> url >> data_file_name >> pos) {
-        DocInfo doc_info(doc_id, url, length, pos);
-        docs_.push_back(doc_info);
+        //DocInfo doc_info(doc_id, url, length, pos);
+        //docs_.push_back(doc_info);
+        docs_[real_doc_num++] = DocInfo(doc_id, url, length, pos);
         total_doc_length += length;
     }
 
     docs_file.close();
+    docs_.resize(real_doc_num);
 
-    average_doc_length_ = total_doc_length / (float)(docs_.size());
-    doc_num_ = docs_.size();
+    average_doc_length_ = total_doc_length / (float)(real_doc_num);
+    doc_num_ = real_doc_num;
+
+    // fill K_
+    float k1 = 1.2, b = 0.75;
+    for (int i = 0; i < K_CACHE_SIZE; i++) {
+        K_[i] = k1 * ((1 - b) + b * (float)i / average_doc_length_);
+    }
 }
 
 bool SearchEngine::open_list(std::string& word, 
@@ -334,7 +446,7 @@ bool SearchEngine::open_list(std::string& word,
         return false;
     }
 
-    posting_list.postings.clear();
+    //posting_list.postings.clear();
     long long start = iter->second.start_position;
     long long end = iter->second.end_position;
     int int_num = (end - start) / sizeof(int);
@@ -343,22 +455,52 @@ bool SearchEngine::open_list(std::string& word,
     // read postings to memory
     inverted_index_file_.seekg(start);
     inverted_index_file_.read((char*)int_buffer, end - start);
+
+    int max_posting_num = int_num/3;
+    int real_posting_num = 0;
+
     int doc_id = 0, freq = 0;
     int index = 0;
+    std::clock_t begin = clock();
+    PostingInfo post;
+    posting_list.postings.resize(max_posting_num, post);
+    //posting_list.postings = std::vector<PostingInfo>(max_posting_num, PostingInfo());
     while (index < int_num) {
         doc_id = int_buffer[index++];
         freq = int_buffer[index++];
-        std::vector<int> positions;
+        std::vector<int> positions(freq);
         for (int i = 0; i < freq; i++) {
-            positions.push_back(int_buffer[index++]);
+            positions[i] = int_buffer[index++];
         }
-        PostingInfo posting_info(doc_id, freq, positions);
-        posting_list.postings.push_back(posting_info);
+        //PostingInfo posting_info(doc_id, freq, positions);
+        //posting_list.postings.push_back(posting_info);
+        posting_list.postings[real_posting_num++] = PostingInfo(doc_id, freq, positions);
     }
-    posting_list.current = -1;
+    posting_list.postings.resize(real_posting_num);
+    std::clock_t end_t = clock();
+    posting_list.current = 0;
+    //std::cout << "[time]: " << (double)(end_t - begin) / (double)CLOCKS_PER_SEC << std::endl;
     
     delete[] int_buffer;
     return true;
+}
+
+int SearchEngine::next_geq_linear(PostingList& posting_list, int k) {
+    //if (posting_list.current < 0) posting_list.current = 0;
+    if (posting_list.current >= posting_list.postings.size()) return doc_num_;
+    if (posting_list.postings[posting_list.current].doc_id >= k) {
+        return posting_list.postings[posting_list.current].doc_id;
+    }
+
+    // linear search
+    while (posting_list.current < posting_list.postings.size() &&
+        posting_list.postings[posting_list.current].doc_id < k) {
+        posting_list.current++;
+    }
+
+    if (posting_list.current >= posting_list.postings.size()) return doc_num_;
+
+    return posting_list.postings[posting_list.current].doc_id;
 }
 
 /*
@@ -369,14 +511,43 @@ bool SearchEngine::open_list(std::string& word,
 int SearchEngine::next_geq(PostingList& posting_list, int k) {
     if (posting_list.current < 0) posting_list.current = 0;
     if (posting_list.current >= posting_list.postings.size()) return doc_num_;
-
-    while (posting_list.current < posting_list.postings.size() &&
-        posting_list.postings[posting_list.current].doc_id < k) {
-        posting_list.current++;
+    if (posting_list.postings[posting_list.current].doc_id >= k) {
+        return posting_list.postings[posting_list.current].doc_id;
     }
 
-    if (posting_list.current >= posting_list.postings.size()) return doc_num_;
-    return posting_list.postings[posting_list.current].doc_id;
+    // binary search
+    int left = posting_list.current;
+    int right = posting_list.postings.size() - 1;
+    int middle = (left + right) / 2;
+    if (posting_list.postings[right].doc_id == k) {
+        posting_list.current = right;
+        return k;
+    }
+    if (posting_list.postings[right].doc_id < k) {
+        posting_list.current = right;
+        return doc_num_;
+    }
+    while (posting_list.postings[middle].doc_id != k && right - left >= 2) {
+        if (posting_list.postings[middle].doc_id > k) {
+            right = middle;
+            middle = (left + right) / 2;
+        } else {
+            left = middle;
+            middle = (left + right) / 2;
+        }
+    }
+    if (posting_list.postings[middle].doc_id == k) {
+        posting_list.current = middle;
+        return posting_list.postings[posting_list.current].doc_id;
+    }
+    if (posting_list.postings[left].doc_id >= k) {
+        posting_list.current = left;
+        return posting_list.postings[posting_list.current].doc_id;
+    } else {
+        posting_list.current = right;
+        return posting_list.postings[posting_list.current].doc_id;
+    }
+    // binary search
 }
 
 int SearchEngine::get_freq(PostingList& posting_list) {
--- search_engine.h
+++ search_engine.h
@@ -4,11 +4,18 @@
 #include <map>
 #include <fstream>
 
+#define K_CACHE_SIZE 1024
+
 struct WordInfo {
     std::string word;
     long long start_position;
     long long end_position;
     int num_of_doc_contain_this_word;
+
+    WordInfo(std::string& word_, long long start_, long long end_, int num_):
+        word(word_), start_position(start_), end_position(end_),
+        num_of_doc_contain_this_word(num_) {}
+    WordInfo() {}
 };
 
 struct DocInfo {
@@ -19,6 +26,7 @@ struct DocInfo {
 
     DocInfo(int doc_id_, std::string url_, int length_, long long pos_):
         doc_id(doc_id_), url(url_), length(length_), start_position(pos_) {}
+    DocInfo() {}
 };
 
 struct PostingInfo {
@@ -28,6 +36,7 @@ struct PostingInfo {
 
     PostingInfo(int doc_id_, int freq_, std::vector<int>& pos_):
         doc_id(doc_id_), freq(freq_), pos(pos_) {}
+    PostingInfo() {};
 };
 
 struct PostingList {
@@ -70,6 +79,14 @@ private:
     std::vector<DocInfo> docs_;
     int doc_num_;
     int top_k_;
+    bool show_running_time_;
+
+    // in bm25 score, the left part is a function of query words,
+    // it's same for different doc, so we can cache this part
+    // this make the disjunctive query "new york university" score
+    // computing time 3.79s->2.13s, total time: 5.65s->3.57s (200m doc)
+    std::map<int, float> bm25_score_left_cache_;
+    float K_[K_CACHE_SIZE];
 
     std::ifstream new_text_file_;
     
@@ -79,6 +96,7 @@ private:
 
     bool open_list(std::string& word, PostingList& posting_list);
     int next_geq(PostingList& posting_list, int k);
+    int next_geq_linear(PostingList& posting_list, int k);
     int get_freq(PostingList& posting_list);
 
     Query get_query();
